b76e5807daf8ac728ecf691176168b145fb026bf
MrP4ce <rizkacandfa88@gmail.com>
2024-07-01 22:30:06 +0700

    Api Pache Testing1

--- start/routes.js
+++ start/routes.js
@@ -16,11 +16,11 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
+Route.get('/index', () => {
   return { greeting: 'Hello world in JSON' }
 })
 
 Route.group(()=>{
   Route.get("/", 'Hongkong/ResultController.getResult')
   
-}).prefix("api/")
+})
